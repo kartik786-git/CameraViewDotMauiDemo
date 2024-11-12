71cdf318fb580f97d346109dfa3362a9ac11936c
kartik <kartik.kumar53@gmail.com>
2024-11-12 12:35:52 +0530

    Add project files.

---
+++ CameraViewDotMauiDemo/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Core.Primitives;
+using Microsoft.Maui;
+
+namespace CameraViewDotMauiDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly ICameraProvider _cameraProvider;
+
+        public MainPage(ICameraProvider cameraProvider)
+        {
+            InitializeComponent();
+            _cameraProvider = cameraProvider;
+        }
+
+        protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+        {
+            base.OnNavigatedTo(args);
+            await _cameraProvider.RefreshAvailableCameras(CancellationToken.None);
+            MyCamera.SelectedCamera = _cameraProvider.AvailableCameras.
+                Where(x => x.Position == CameraPosition.Rear).FirstOrDefault();
+
+            
+        }
+
+        protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
+        {
+            base.OnNavigatedFrom(args);
+            MyCamera.MediaCaptured -= MyCamera_MediaCaptured;
+            MyCamera.Handler?.DisconnectHandler();
+        }
+
+        private void MyCamera_MediaCaptured(object? sender , CommunityToolkit.Maui.Views.MediaCapturedEventArgs e)
+        {
+            if (Dispatcher.IsDispatchRequired)
+            {
+                Dispatcher.Dispatch(() => MyImage.Source = ImageSource.FromStream(() => e.Media));
+                return;
+            }
+                MyImage.Source = ImageSource.FromStream(() => e.Media);
+        }
+
+        private async void Capture_Image(object sender, EventArgs e)
+        {
+            await MyCamera.CaptureImage(CancellationToken.None);
+        }
+
+        private async void Back_Camera(object sender, EventArgs e)
+        {
+            await _cameraProvider.RefreshAvailableCameras(CancellationToken.None);
+            MyCamera.SelectedCamera = _cameraProvider.AvailableCameras.
+                Where(x => x.Position == CameraPosition.Rear).FirstOrDefault();
+        }
+
+        private async void Front_Camera(object sender, EventArgs e)
+        {
+            await _cameraProvider.RefreshAvailableCameras(CancellationToken.None);
+            MyCamera.SelectedCamera = _cameraProvider.AvailableCameras.
+                Where(x => x.Position == CameraPosition.Front).FirstOrDefault();
+        }
+
+        private void Flash_On_Off(object sender, EventArgs e)
+        {
+            MyCamera.CameraFlashMode = MyCamera.CameraFlashMode == CameraFlashMode.Off 
+                ? CameraFlashMode.On : CameraFlashMode.Off;
+        }
+
+        private void Zoom_In(object sender, EventArgs e)
+        {
+            MyCamera.ZoomFactor += 0.1f;
+        }
+    }
+
+}
---
+++ CameraViewDotMauiDemo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CameraViewDotMauiDemo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
